69ae0e072c1ce6ecbdd4e0943b13730a8b909950
Great Dolphin <great.dolphin.ls@gmail.com>
2021-05-04 01:32:56 -0700

    [feat] implement wallet-connect feature

---
+++ libs/web3-connectors.js
@@ -0,0 +1,31 @@
+import {
+  InjectedConnector,
+  NoEthereumProviderError,
+  UserRejectedRequestError as UserRejectedRequestErrorInjected
+} from '@web3-react/injected-connector'
+import { UnsupportedChainIdError } from '@web3-react/core'
+
+import MESSAGES from 'utils/constants/messages'
+
+const injected = new InjectedConnector({
+  supportedChainIds: [1]
+});
+
+const getErrorMessage = (error) => {
+  if (error instanceof NoEthereumProviderError) {
+    return MESSAGES.CONNECT_NO_ETHEREUM_PROVIDER_ERROR
+  } else if (error instanceof UnsupportedChainIdError) {
+    return MESSAGES.CONNECT_UNSUPPORTED_CHAIN_ID_ERROR
+  } else if (
+    error instanceof UserRejectedRequestErrorInjected
+  ) {
+    return MESSAGES.CONNECT_ACCESS_BINANCE_ERROR
+  } else {
+    return MESSAGES.CONNECT_UNKNOWN_ERROR
+  }
+}
+
+export {
+  injected,
+  getErrorMessage
+}
