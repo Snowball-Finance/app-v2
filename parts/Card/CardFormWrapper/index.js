75a9922ecf247501135c8d57501e3cb6bfe8db40
Great Dolphin <great.dolphin.ls@gmail.com>
2021-05-25 09:47:47 -0700

    [feat] create my share card

--- parts/Card/CardFormWrapper/index.js
+++ parts/Card/CardFormWrapper/index.js
@@ -8,17 +8,25 @@ const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(4, 2.5, 3.5),
     width: '100%',
+    height: '100%',
     maxWidth: theme.custom.layout.maxCardWidth
+  },
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginBottom: theme.spacing(2)
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: theme.spacing(2.5)
+    marginBottom: theme.spacing(0.5)
   }
 }));
 
 const CardFormWrapper = ({
   title,
+  subTitle,
+  icon,
   className,
   children
 }) => {
@@ -26,11 +34,21 @@ const CardFormWrapper = ({
 
   return (
     <Card className={clsx(classes.root, className)}>
-      {title && (
-        <Typography className={classes.title}>
-          {title}
-        </Typography>
-      )}
+      <div className={classes.header}>
+        <div>
+          {title && (
+            <Typography className={classes.title}>
+              {title}
+            </Typography>
+          )}
+          {subTitle && (
+            <Typography variant='body2'>
+              {subTitle}
+            </Typography>
+          )}
+        </div>
+        {icon && icon}
+      </div>
       {children}
     </Card>
   )
