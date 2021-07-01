a98a2ed7b35690c960be1fb5910fabb387105920
luis-talavera <talaverameza@gmail.com>
2021-07-01 16:14:03 -0400

    Disable Elevation for TopAppBar component
    Change background size cover on Compound and Earn page

--- parts/Compound/CompoundHeader/index.js
+++ parts/Compound/CompoundHeader/index.js
@@ -1,82 +1,73 @@
-
 import { memo } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import { Card, Typography } from '@material-ui/core'
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
 import clsx from 'clsx'
 
 import { COMPOUND_AND_EARN_BACKGROUND_IMAGE_PATH } from 'utils/constants/image-paths'
 import ContainedButton from 'components/UI/Buttons/ContainedButton'
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    width: '100%',
-    height: 100,
-    backgroundImage: `url(${COMPOUND_AND_EARN_BACKGROUND_IMAGE_PATH})`
-  },
-  icon: {
-    position: 'absolute',
-    right: 40,
-    bottom: 0,
-    objectFit: 'contain',
-    height: 100,
-  },
-  container: {
-    marginLeft: theme.spacing(4)
-  },
-  header: {
-    fontWeight: 'bold',
-    color: theme.custom.palette.white,
-    marginBottom: theme.spacing(1)
-  },
-  subHeaderButton: {
-    backgroundColor: 'rgba(255, 255, 255, 0.12)',
-    fontSize: 12,
-    fontWeight: 600,
-    color: theme.custom.palette.white,
-    textTransform: 'none'
-  },
-  rightIcon: {
-    backgroundColor: theme.custom.palette.blue,
-    borderRadius: '50%'
-  },
-}));
+    root: {
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        width: '100%',
+        height: 100,
+        backgroundImage: `url(${COMPOUND_AND_EARN_BACKGROUND_IMAGE_PATH})`,
+        backgroundSize: 'cover',
+    },
+    icon: {
+        position: 'absolute',
+        right: 40,
+        bottom: 0,
+        objectFit: 'contain',
+        height: 100,
+    },
+    container: {
+        marginLeft: theme.spacing(4),
+    },
+    header: {
+        fontWeight: 'bold',
+        color: theme.custom.palette.white,
+        marginBottom: theme.spacing(1),
+    },
+    subHeaderButton: {
+        backgroundColor: 'rgba(255, 255, 255, 0.12)',
+        fontSize: 12,
+        fontWeight: 600,
+        color: theme.custom.palette.white,
+        textTransform: 'none',
+    },
+    rightIcon: {
+        backgroundColor: theme.custom.palette.blue,
+        borderRadius: '50%',
+    },
+}))
 
-const CompoundHeader = ({
-  title,
-  subHeader,
-  icon,
-  className
-}) => {
-  const classes = useStyles();
+const CompoundHeader = ({ title, subHeader, icon, className }) => {
+    const classes = useStyles()
 
-  return (
-    <Card className={clsx(classes.root, className)}>
-      <img
-        alt='icon'
-        src={icon}
-        className={classes.icon}
-      />
-      <div className={classes.container}>
-        <Typography variant='h5' className={classes.header}>
-            {title}
-        </Typography>
-        <ContainedButton
-            className={classes.subHeaderButton}
-            size='small'
-            disableElevation
-            endIcon={<ChevronRightIcon className={classes.rightIcon} />}
-        >
-            {subHeader}
-        </ContainedButton>
-      </div>
-    </Card>
-  )
+    return (
+        <Card className={clsx(classes.root, className)}>
+            <img alt="icon" src={icon} className={classes.icon} />
+            <div className={classes.container}>
+                <Typography variant="h5" className={classes.header}>
+                    {title}
+                </Typography>
+                <ContainedButton
+                    className={classes.subHeaderButton}
+                    size="small"
+                    disableElevation
+                    endIcon={<ChevronRightIcon className={classes.rightIcon} />}
+                >
+                    {subHeader}
+                </ContainedButton>
+            </div>
+        </Card>
+    )
 }
 
 export default memo(CompoundHeader)
--- styles/theme.js
+++ styles/theme.js
@@ -1,151 +1,156 @@
-
-import {
-  createMuiTheme,
-  responsiveFontSizes
-} from '@material-ui/core/styles'
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles'
 
 const fontFamily = [
-  'Montserrat',
-  'Rubik',
-  '-apple-system',
-  'BlinkMacSystemFont',
-  '"Segoe UI"',
-  'Arial',
-  'sans-serif',
-  '"Apple Color Emoji"',
-  '"Segoe UI Emoji"',
-  '"Segoe UI Symbol"',
+    'Montserrat',
+    'Rubik',
+    '-apple-system',
+    'BlinkMacSystemFont',
+    '"Segoe UI"',
+    'Arial',
+    'sans-serif',
+    '"Apple Color Emoji"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"',
 ]
 
-const lightTheme = responsiveFontSizes(createMuiTheme({
-  typography: {
-    fontFamily: fontFamily.join(',')
-  },
-  overrides: {
-    MuiCssBaseline: {
-      '@global': {
-        '@font-face': [{
-          fontFamily: 'Montserrat',
-          fontStyle: 'normal',
-          fontDisplay: 'swap',
-          fontWeight: 400,
-          src: `
-            local('Montserrat'),
-            url('/assets/fonts/Montserrat.woff') format('woff')`
-        }],
-      }
-    },
-    MuiCard: {
-      root: {
-        boxShadow: '1px 2px 5px rgba(117, 115, 115, 0.2)',
-        borderRadius: 10,
-      }
-    },
-  },
-  palette: {
-    primary: {
-      main: '#0085EB'
-    },
-    secondary: {
-      main: '#B9B9C3',
-      contrastText: '#ffffff'
-    },
-    danger: {
-      main: '#eb196e',
-    },
-    background: {
-      default: '#E5E5E5',
-      primary: '#FFFFFF'
-    },
-    text: {
-      primary: '#6E6B7B',
-      secondary: '#28C76F',
-    },
-  },
-  custom: {
-    palette: {
-      white: '#FFFFFF',
-      lightBlue: '#DBEDFF',
-      blue: '#1A93F2',
-      darkBlue: '#1E2848',
-      green: '#28C76F',
-      darkGreen: '#2A4428',
-      border: '#D8D6DE'
-    },
-    layout: {
-      maxCardWidth: 513,
-      maxDesktopWidth: 1550,
-      openDrawerWidth: 240,
-      closedDrawerWidth: 57
-    },
-  }
-}));
+const lightTheme = responsiveFontSizes(
+    createMuiTheme({
+        typography: {
+            fontFamily: fontFamily.join(','),
+        },
+        overrides: {
+            MuiCssBaseline: {
+                '@global': {
+                    '@font-face': [
+                        {
+                            fontFamily: 'Montserrat',
+                            fontStyle: 'normal',
+                            fontDisplay: 'swap',
+                            fontWeight: 400,
+                            src: `local('Montserrat'),
+                          url('/assets/fonts/Montserrat.woff') format('woff')`,
+                        },
+                    ],
+                },
+            },
+            MuiCard: {
+                root: {
+                    boxShadow: '1px 2px 5px rgba(117, 115, 115, 20%)',
+                    borderRadius: 10,
+                },
+            },
+            MuiPaper: {
+                rounded: {
+                    borderRadius: 10,
+                },
+            },
+        },
+        palette: {
+            primary: {
+                main: '#0085EB',
+            },
+            secondary: {
+                main: '#B9B9C3',
+                contrastText: '#ffffff',
+            },
+            danger: {
+                main: '#eb196e',
+            },
+            background: {
+                default: '#E5E5E5',
+                primary: '#FFFFFF',
+            },
+            text: {
+                primary: '#6E6B7B',
+                secondary: '#28C76F',
+            },
+        },
+        custom: {
+            palette: {
+                white: '#FFFFFF',
+                lightBlue: '#DBEDFF',
+                blue: '#1A93F2',
+                darkBlue: '#1E2848',
+                green: '#28C76F',
+                darkGreen: '#2A4428',
+                border: '#D8D6DE',
+            },
+            layout: {
+                maxCardWidth: 513,
+                maxDesktopWidth: 1550,
+                openDrawerWidth: 240,
+                closedDrawerWidth: 57,
+            },
+        },
+    })
+)
 
-const darkTheme = responsiveFontSizes(createMuiTheme({
-  typography: {
-    fontFamily: fontFamily.join(',')
-  },
-  overrides: {
-    MuiCssBaseline: {
-      '@global': {
-        '@font-face': [{
-          fontFamily: 'Montserrat',
-          fontStyle: 'normal',
-          fontDisplay: 'swap',
-          fontWeight: 400,
-          src: `
+const darkTheme = responsiveFontSizes(
+    createMuiTheme({
+        typography: {
+            fontFamily: fontFamily.join(','),
+        },
+        overrides: {
+            MuiCssBaseline: {
+                '@global': {
+                    '@font-face': [
+                        {
+                            fontFamily: 'Montserrat',
+                            fontStyle: 'normal',
+                            fontDisplay: 'swap',
+                            fontWeight: 400,
+                            src: `
             local('Montserrat'),
-            url('/assets/fonts/Montserrat.woff') format('woff')`
-        }],
-      }
-    },
-    MuiCard: {
-      root: {
-        boxShadow: '1px 2px 5px rgba(117, 115, 115, 0.2)',
-        borderRadius: 10,
-      }
-    },
-  },
-  palette: {
-    primary: {
-      main: '#337ab7',
-    },
-    secondary: {
-      main: '#B9B9C3',
-      contrastText: '#ffffff'
-    },
-    danger: {
-      main: '#eb196e',
-    },
-    background: {
-      default: '#1e2644',
-      primary: '#1c2132',
-    },
-    text: {
-      primary: '#FFFFFF',
-      secondary: '#337ab7'
-    },
-  },
-  custom: {
-    palette: {
-      white: '#FFFFFF',
-      lightBlue: '#DBEDFF',
-      blue: '#1A93F2',
-      darkBlue: '#1E2848',
-      green: '#28C76F',
-      darkGreen: '#2A4428',
-      border: '#D8D6DE'
-    },
-    layout: {
-      maxCardWidth: 513,
-      maxDesktopWidth: 1550,
-      openDrawerWidth: 240,
-      closedDrawerWidth: 57
-    },
-  }
-}));
+            url('/assets/fonts/Montserrat.woff') format('woff')`,
+                        },
+                    ],
+                },
+            },
+            MuiCard: {
+                root: {
+                    boxShadow: '1px 2px 5px rgba(117, 115, 115, 0.2)',
+                    borderRadius: 10,
+                },
+            },
+        },
+        palette: {
+            primary: {
+                main: '#337ab7',
+            },
+            secondary: {
+                main: '#B9B9C3',
+                contrastText: '#ffffff',
+            },
+            danger: {
+                main: '#eb196e',
+            },
+            background: {
+                default: '#1e2644',
+                primary: '#1c2132',
+            },
+            text: {
+                primary: '#FFFFFF',
+                secondary: '#337ab7',
+            },
+        },
+        custom: {
+            palette: {
+                white: '#FFFFFF',
+                lightBlue: '#DBEDFF',
+                blue: '#1A93F2',
+                darkBlue: '#1E2848',
+                green: '#28C76F',
+                darkGreen: '#2A4428',
+                border: '#D8D6DE',
+            },
+            layout: {
+                maxCardWidth: 513,
+                maxDesktopWidth: 1550,
+                openDrawerWidth: 240,
+                closedDrawerWidth: 57,
+            },
+        },
+    })
+)
 
-export {
-  lightTheme,
-  darkTheme
-};
+export { lightTheme, darkTheme }
