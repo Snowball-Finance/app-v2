1235f2be5d32ea651c4cd6d121657b68a1926ad9
Frostbourne <frostbournesb@protonmail.com>
2021-08-05 20:27:48 -0700

    Fix Get PGL Link

--- utils/helpers/getProperAction.js
+++ utils/helpers/getProperAction.js
@@ -1,6 +1,6 @@
 import LINKS from 'utils/constants/links'
+import { WAVAX } from 'utils/constants/addresses'
 
-const WAVAX = '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7';
 const PangolinPoolURL = 'https://app.pangolin.exchange/#/add/';
 const JoePoolURL = 'https://www.traderjoexyz.com/#/pool/';
 
