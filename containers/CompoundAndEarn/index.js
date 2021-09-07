94d513e49dd68ead28e6be1989b067edc1479cd3
marlon monroy <marlon@arkius.org>
2021-09-06 18:02:24 -0700

    fix containers/CompoundAndEarn/index.js

--- containers/CompoundAndEarn/index.js
+++ containers/CompoundAndEarn/index.js
@@ -40,12 +40,8 @@ const CompoundAndEarn = () => {
   const { account } = useWeb3React();
   const { getLastSnowballInfo } = useAPIContext();
   const snowballInfoQuery = getLastSnowballInfo();
-<<<<<<< HEAD
-  const { userPools, userDeprecatedPools } = useCompoundAndEarnContract();
-=======
   const { userPools, userDeprecatedPools, loadedDeprecated,
     sortedUserPools,setLoadedDeprecated,setSortedUserPools } = useCompoundAndEarnContract();
->>>>>>> c7c76fef1fc65760e1725c84a675bf570fde0b3a
 
   const [search, setSearch] = useState('');
   const [type, setType] = useState('apy');
@@ -56,11 +52,7 @@ const CompoundAndEarn = () => {
   const [filterDataByProtocol, setFilterDataByProtocol] = useState([]);
 
   useEffect(() => {
-<<<<<<< HEAD
-    if(userDeprecatedPools.length > 0 && !loadedDeprecated){
-=======
     if(userDeprecatedPools.length > 0 && !loadedDeprecated && sortedUserPools){
->>>>>>> c7c76fef1fc65760e1725c84a675bf570fde0b3a
       let newArray = [...lastSnowballInfo];
       userDeprecatedPools.forEach((pool) => {
         newArray.unshift(pool);
@@ -68,13 +60,8 @@ const CompoundAndEarn = () => {
       setLastSnowballInfo(newArray);
       setLoadedDeprecated(true);
     }
-<<<<<<< HEAD
-
-  },[userDeprecatedPools]);
-=======
      // eslint-disable-next-line react-hooks/exhaustive-deps
   },[userDeprecatedPools,loadedDeprecated,sortedUserPools]);
->>>>>>> c7c76fef1fc65760e1725c84a675bf570fde0b3a
 
   useEffect(() => {
     const { data: { LastSnowballInfo: { poolsInfo = [] } = {} } = {} } = snowballInfoQuery;
