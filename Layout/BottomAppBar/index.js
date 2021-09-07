c3f40051b2071531756a286a03aedcbfabfae975
marlon monroy <marlon@arkius.org>
2021-09-06 18:27:25 -0700

    remove added imports

--- Layout/BottomAppBar/index.js
+++ Layout/BottomAppBar/index.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles'
 
 import ConnectWallet from '../Shared/ConnectWallet'
 import SnobBalance from '../Shared/SnobBalance'
-import SnobPrice from '../Shared/SnobPrice'
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
