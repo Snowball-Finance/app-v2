edce2e49767f1cf1d2f97f70d0a7edd10a788529
Techlord <dev@techlord.net>
2021-09-09 19:46:10 -0400

    implement coin98 wallet

--- components/WalletModal/index.js
+++ components/WalletModal/index.js
@@ -1,21 +1,25 @@
 
-import { memo, useMemo } from 'react'
+import { memo, useMemo, useState } from 'react'
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { Grid, useMediaQuery } from '@material-ui/core'
 import { useWeb3React } from '@web3-react/core'
 
+import SnowConfirmDialog from 'parts/SnowConfirmDialog'
 import SnowDialog from 'components/SnowDialog'
 import WalletCard from 'components/WalletModal/WalletCard'
 import { walletlink, injected } from 'utils/constants/connectors'
+import ANIMATIONS from 'utils/constants/animate-icons';
 
 const DESKTOP_CONNECTORS = {
   'MetaMask': injected,
   'Coinbase Wallet': walletlink,
+  'Coin98': injected,
 }
 
 const MOBILE_CONNECTORS = {
   'MetaMask': injected,
   'Coinbase Wallet': walletlink,
+  'Coin98': injected,
 }
 
 const useStyles = makeStyles(() => ({
@@ -36,13 +40,21 @@ const WalletModal = ({
   const isSm = useMediaQuery(theme.breakpoints.down('sm'), { defaultMatches: true });
 
   const walletConnectors = useMemo(() => isSm ? MOBILE_CONNECTORS : DESKTOP_CONNECTORS, [isSm]);
+  const [openModal, setOpen] = useState(false)
 
-  const walletSelectHandler = (currentConnector) => {
+  const walletSelectHandler = (currentConnector, name) => {
+    if(name == 'Coin98') {
+      if (!window.ethereum.isCoin98 && !window.coin98) {
+        setOpen(true)
+        return;
+      }
+    }
     onConnectWallet(currentConnector);
     onClose();
   }
 
   return (
+    <>
     <SnowDialog
       open={open}
       onClose={onClose}
@@ -52,7 +64,7 @@ const WalletModal = ({
         {Object.keys(walletConnectors).map(name => {
           const currentConnector = walletConnectors[name]
           return (
-            <Grid key={name} item xs={12} onClick={() => walletSelectHandler(currentConnector)}>
+            <Grid key={name} item xs={12} onClick={() => walletSelectHandler(currentConnector, name)}>
               <WalletCard
                 selected={currentConnector === connector}
                 name={name}
@@ -62,6 +74,14 @@ const WalletModal = ({
         })}
       </Grid>
     </SnowDialog>
+    <SnowConfirmDialog
+          open={openModal}
+          onClose={() => setOpen(false)}
+          title='Warning'
+          text="You need to install Coin98 Wallet Extention"
+          icon={ANIMATIONS.WARNING.VALUE}
+        />
+    </>
   );
 }
 
