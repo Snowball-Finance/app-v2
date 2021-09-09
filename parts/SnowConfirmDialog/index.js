56b2c7148bac3d171d7a6063dd471903621b71d0
Techlord <dev@techlord.net>
2021-09-09 19:26:24 -0400

    DEV-161

--- parts/SnowConfirmDialog/index.js
+++ parts/SnowConfirmDialog/index.js
@@ -1,22 +1,30 @@
 
 import { memo } from 'react'
 import { Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
 
 import SnowDialog from 'components/SnowDialog'
 import SnowDynamicLottie from 'components/SnowDynamicLottie'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    overflowWrap: 'break-word'
+  },
+}));
 
 const SnowConfirmDialog = ({
   text = 'Are you sure to proceed this operation?',
   icon = '',
   ...rest
 }) => {
+  const classes = useStyles();
   return (
     <SnowDialog {...rest}>
       <SnowDynamicLottie
         show={true}
         size={150}
         icon={icon} />
-      <Typography color='textPrimary' variant='h6' align='center'>
+      <Typography color='textPrimary' variant='h6' align='center' className={classes.root}>
         {text}
       </Typography>
     </SnowDialog>
