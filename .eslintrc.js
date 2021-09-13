80f41dcdf882a0f25890aa41571c776056e7ffb7
Sanjeev Sharma <shnjeevsharma@gmail.com>
2021-09-13 21:30:44 +0530

    Modified the eslint file Added prettier file Added vscode folder with setting and extension file

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,24 +5,32 @@
  */
 
 module.exports = {
-  'parser': 'babel-eslint',
-  'parserOptions': {
-    'ecmaVersion': 2018,
-    'sourceType': 'module'
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  'env': {
-    'es6': true
+  env: {
+    es6: true,
   },
-  'extends': [
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended'
-  ],
-  'rules': {
-    'react/display-name': 0,
-    'no-unused-vars': 'warn',
-    'react/no-unknown-property': 0,
-    'react/prop-types': 0,
-    'react/react-in-jsx-scope': 0,
-    'react-hooks/rules-of-hooks': 0
-  }
+  extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended', 'airbnb', 'prettier'],
+  rules: {
+    'react/display-name': 0, // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/display-name.md
+    'no-unused-vars': 'warn', // When we use var, will give us warning
+    'react/no-unknown-property': 0, // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unknown-property.md
+    'react/prop-types': 0, // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md
+    'react/react-in-jsx-scope': 0, // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
+    'react-hooks/rules-of-hooks': 0, // https://reactjs.org/docs/hooks-rules.html
+    quotes: [2, 'single', 'avoid-escape'],
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'any',
+        ignoreReadBeforeAssign: false,
+      },
+    ], // https://eslint.org/docs/rules/prefer-const
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], // Allow both file with extension .js and .jsx
+    'max-len': [1, 100, 2, { ignoreComments: true }], // Allow 100 max char in a line, ignore comments
+    'react/jsx-props-no-spreading': 'off', // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-props-no-spreading.md
+  },
 };
