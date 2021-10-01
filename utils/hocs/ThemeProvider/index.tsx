b19929a13afb04572bb1faf850001ac85d858d58
prodvdjin <pro.davidjin.dev@gmail.com>
2021-10-01 01:52:02 -0700

    updated ThemeProvider and SnowWeb3Provider with typescript

--- utils/hocs/ThemeProvider/index.tsx
+++ utils/hocs/ThemeProvider/index.tsx
@@ -1,13 +1,14 @@
 
-import { memo } from 'react'
+import React, { memo } from 'react'
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 
 import { useDarkMode } from 'contexts/ui-context'
 import { lightTheme, darkTheme } from 'styles/theme'
+import { ChildrenProps } from 'types'
 
 const ThemeProvider = ({
   children
-}) => {
+}: ChildrenProps): JSX.Element => {
   const { darkMode } = useDarkMode();
   const theme = darkMode ? darkTheme : lightTheme;
 
