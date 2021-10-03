2d78250025722f9ea0402621e4565c83c85dd5ac
prodvdjin <pro.davidjin.dev@gmail.com>
2021-10-02 20:37:28 -0700

    updated hook(useInactiveListener) and styles to typescript

--- utils/hooks/useInactiveListener.ts
+++ utils/hooks/useInactiveListener.ts
@@ -7,21 +7,21 @@ const useInactiveListener = (suppress = false) => {
   const { active, error, activate } = useWeb3React();
 
   useEffect(() => {
-    const { ethereum } = window;
+    const ethereum = window['ethereum'];
     if (ethereum && ethereum.on && !active && !error && !suppress) {
-      const handleChainChanged = chainId => {
+      const handleChainChanged = (chainId: number) => {
         console.log('chainChanged', chainId);
         activate(injected);
       };
 
-      const handleAccountsChanged = accounts => {
+      const handleAccountsChanged = (accounts: Array<any>) => {
         console.log('accountsChanged', accounts);
         if (accounts.length > 0) {
           activate(injected);
         }
       };
 
-      const handleNetworkChanged = networkId => {
+      const handleNetworkChanged = (networkId: number) => {
         console.log('networkChanged', networkId);
         activate(injected);
       };
