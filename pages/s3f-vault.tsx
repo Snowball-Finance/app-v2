afdfc903a8ea820917e60b55690a36d394f3a26c
prodvdjin <pro.davidjin.dev@gmail.com>
2021-10-02 21:58:12 -0700

    updated pages from js to tsx

--- pages/s3f-vault.tsx
+++ pages/s3f-vault.tsx
@@ -2,7 +2,7 @@
 import S3fVault from 'containers/StableVault/S3fVault'
 import { S3fVaultContractProvider } from 'contexts/s3f-vault-context'
 
-export default function S3fVaultPage() {
+export default function S3fVaultPage(): JSX.Element {
   return (
     <S3fVaultContractProvider>
       <S3fVault />
--- pages/staking.tsx
+++ pages/staking.tsx
@@ -1,7 +1,7 @@
 
 import Staking from 'containers/Staking'
 
-export default function StakingPage() {
+export default function StakingPage(): JSX.Element {
   return (
     <Staking />
   )
