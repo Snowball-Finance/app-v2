d8aefede4efc8eddc498ce96ef1d45d6a870cbd9
mohsen <mhsnprvr4@gmail.com>
2021-11-03 00:48:41 +0330

    minor refactor on arrowDown icon

--- components/Icons/ArrowDownIcon/index.js
+++ components/Icons/ArrowDownIcon/index.js
@@ -1,25 +1,15 @@
 
 import { memo } from 'react'
 import SvgIcon from '@material-ui/core/SvgIcon'
-import { makeStyles } from '@material-ui/core/styles'
-import clsx from 'clsx'
-
-const useStyles = makeStyles(() => ({
-  root: {
-    // width: 24,
-    // height: 24
-  }
-}));
 
 const ArrowDownIcon = ({
   className,
   viewBox,
   ...rest
 }) => {
-  const classes = useStyles();
 
   return (
-    <SvgIcon viewBox={viewBox || '0 0 24 24'} {...rest} className={clsx(classes.root, className)}>
+    <SvgIcon viewBox={viewBox || '0 0 24 24'} {...rest} {...{ className }}>
       <path d="m20 12-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"></path>
     </SvgIcon>
   )
