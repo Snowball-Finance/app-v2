c867c06916bc417df135ccce669931f4e6847a9e
Jonasslv <jowsba@hotmail.com>
2021-11-24 19:43:20 -0300

    fix skeleton on search

--- containers/CompoundAndEarn/index.js
+++ containers/CompoundAndEarn/index.js
@@ -269,7 +269,7 @@ const CompoundAndEarn = () => {
             PAIRS
           </Typography>
         </Grid>
-        {!lastSnowballInfo.length > 0 || transactionUpdateLoading || snowballInfoQuery.loading
+        {transactionUpdateLoading || snowballInfoQuery.loading
           ? (
             <Grid item xs={12}>
               <CompoundAndEarnSkeleton />
