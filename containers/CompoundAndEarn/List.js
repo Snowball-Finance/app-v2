940db322b15efd173dfda1799104e8a00f54ff4f
Sanjeev Sharma <53838714+Sanjeev1308@users.noreply.github.com>
2021-11-24 23:24:36 +0530

    Bug/pools order (#347)
    * added Teddy Cash, updated tags, filter option, theming (#345)
    
    * Worked on the tooltip issue on dark mode (#305)
    
    * Worked on the tooltip issue on dark mode
    
    * Resolved the comment issue
    
    * Feat/david/compound and earn card redesign (#335)
    
    * updated list item detail on C&E page
    
    * updated compound and earn page styling and logic
    
    * fixed button layout on C&E card
    
    * hide underlyingTokens on stable valut
    
    * refactored CompoundListDetail/Total component
    
    * validated with optional chaining in CompoundListDetail/Total component
    
    * added validation, handled issues from deprecated pools like PNG-ELE
    
    * refactored SnobApyCalculation component
    
    * fixed underlying token symbol issue
    
    * updated deposit flow (enabled reject and continuing deposit at step 1) (#343)
    
    * Resolved the pools order issue with respect to infinite scroll
    
    * correct token1 bug (#349)
    
    * improved the performance on c&e page (#352)
    
    * updated cchain explorer to snowtrace.io (#357)
    
    * fixed typo for deprecated pool label (#356)
    
    * handled partial withdraw issue (#344)
    
    * handled partial withdraw issue
    
    * updated withdrawal steps parts on withdraw modal
    
    * fixed withdraw modal amount issue
    
    * enabled partial withdraw (handled conflict)
    
    * enable withdraw button from snowglobe on CompoundWithdrawDialogs
    
    * handled the case of nothing in gauge on withdraw modal
    
    * added validation for withdraw modal
    
    * handled the case of image server offlined, optimized no-image
    
    * removed unused blurDataURL stuff
    
    * fixed more info button link issue on governance page
    
    * Task/david/withdraw steps (#359)
    
    * handled partial withdraw issue
    
    * updated withdrawal steps parts on withdraw modal
    
    * fixed withdraw modal amount issue
    
    * updated withdraw step flow, steps parts in Withdraw dialog
    
    * added axial link to stablevault sidebar (#362)
    
    * added axial pools, updated tags, filter option, theming (#364)
    
    * added axial pools, updated tags, filter option, theming
    
    * updated the logo for AXIAL LP icon
    
    * fix jlp button (#365)
    
    * Send 3085 request after connecting (#369)
    
    * send 3085 request after connecting
    
    (cherry picked from commit 08c1a8b36f647f4fd50edf60867bf5de51b34b16)
    
    * remove logs
    
    (cherry picked from commit 1aa8c11a1808e835eb0b9f0e47be027c6625b3ea)
    
    * revert yarn.lock changes
    
    (cherry picked from commit 2bd4d87c3b07f553483d4ef2ce34ee10a3a753d0)
    
    * EOF
    
    (cherry picked from commit 4eb2e3fa8fa907c070378d3fc3347ba744f2af27)
    
    Co-authored-by: Brendan Weinstein <brendan.weinstein@coinbase.com>
    
    * fix decimals when depositing or withdrawing (#371)
    
    Co-authored-by: David J <78897434+prodvdjin@users.noreply.github.com>
    Co-authored-by: Jonas <20801365+Jonasslv@users.noreply.github.com>
    Co-authored-by: prodvdjin <pro.davidjin.dev@gmail.com>
    Co-authored-by: Joshua Lelon Mitchell <jlelonmitchell@gmail.com>
    Co-authored-by: Brendan Weinstein <brendan.weinstein@coinbase.com>

--- containers/CompoundAndEarn/List.js
+++ containers/CompoundAndEarn/List.js
@@ -1,76 +1,72 @@
-import dynamic from 'next/dynamic'
 import React, { memo, useState, useRef, useEffect, useCallback } from 'react';
-import Loading from 'components/Skeletons/pool';
-import Skeleton from 'components/Skeletons/CompoundAndEarn'
-const ListItem = dynamic(() => import('./ListItem'), { loading: () => <Loading /> })
+import dynamic from 'next/dynamic';
 import { Grid } from '@material-ui/core';
+import Loading from 'components/Skeletons/pool';
+import Skeleton from 'components/Skeletons/CompoundAndEarn';
+
+const ListItem = dynamic(() => import('./ListItem'), {
+  loading: () => <Loading />,
+});
 
 const ITEMS_PER_PAGE = 10;
 
 const List = ({ pools, modal, setModal }) => {
-  const [items, setItems] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [initialLoad, setinitialLoad] = useState(true)
-  const hasMore = items.length < pools.length
-  const [page, setPage] = useState(0)
-  const maxPage = Math.ceil(pools.length / ITEMS_PER_PAGE)
-  const contentRef = useRef(null)
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const hasMore = items.length < pools.length;
+  const [page, setPage] = useState(1);
+  const maxPage = Math.ceil(pools.length / ITEMS_PER_PAGE);
+  const contentRef = useRef(null);
 
   const isBottom = useCallback((ref) => {
     if (!ref.current) return false;
-    return ref.current.getBoundingClientRect().bottom <= window.innerHeight
-  },[])
+    return ref.current.getBoundingClientRect().bottom <= window.innerHeight;
+  }, []);
 
   const loadMore = useCallback(() => {
-    const next = (page + 1) % maxPage
-    const nexItems = pools.slice(next * ITEMS_PER_PAGE, ITEMS_PER_PAGE * (next + 1))
-    setLoading(true)
-    setItems([...items, ...nexItems])
-    setPage(next)
-    setLoading(false)
-  },[maxPage,page, setPage, items])
+    if (maxPage >= page) {
+      const nexItems = pools.slice(
+        page * ITEMS_PER_PAGE,
+        ITEMS_PER_PAGE * (page + 1)
+      );
+      setLoading(true);
+      setItems([...items, ...nexItems]);
+      setPage((prev) => prev + 1);
+      setLoading(false);
+    }
+  }, [maxPage, page, setPage, items, pools]);
 
   useEffect(() => {
-    setPage(0)
-    setItems([])
-    setinitialLoad(true)
-  },[pools])
-
-  useEffect(()=> {
-    if(initialLoad) {
-      loadMore();
-      setinitialLoad(false)
-    }
-  }, [initialLoad, setinitialLoad, loadMore])
+    setPage(1);
+    setItems(pools.slice(0, ITEMS_PER_PAGE));
+  }, [pools]);
 
   useEffect(() => {
     const onScroll = () => {
       if (!loading && hasMore && isBottom(contentRef)) {
         loadMore();
       }
-    }
-    document.addEventListener('scroll', onScroll)
-    return () => document.removeEventListener('scroll', onScroll)
-  }, [loadMore, loading, initialLoad, hasMore, page, items, isBottom])
+    };
+    document.addEventListener('scroll', onScroll);
+    return () => document.removeEventListener('scroll', onScroll);
+  }, [loadMore, loading, hasMore, page, items, isBottom]);
 
   return (
     <>
       <div ref={contentRef}>
-        <Grid container spacing={2} >
+        <Grid container spacing={2}>
           {items.map((pool) => (
             <Grid item key={pool.address} xs={12}>
-              {(!pool.deprecatedPool || !(pool.withdrew && pool.claimed)) &&
-                <ListItem pool={pool} modal={modal} setModal={setModal} />}
+              {(!pool.deprecatedPool || !(pool.withdrew && pool.claimed)) && (
+                <ListItem pool={pool} modal={modal} setModal={setModal} />
+              )}
             </Grid>
-          ))
-          }
-
+          ))}
         </Grid>
-        {hasMore && <Skeleton/>}
+        {hasMore && <Skeleton />}
       </div>
-
     </>
-  )
-}
+  );
+};
 
-export default memo(List)
+export default memo(List);
