52526e95a028285ec0e9fee462b274498265ef59
Sanjeev Sharma <53838714+Sanjeev1308@users.noreply.github.com>
2022-02-03 22:49:43 +0530

    Worked on the swap apr tooltip design (#433)
    Added header swap APR on tooltip

--- parts/Compound/CompoundListDetail/SwapAPRInfo/SwapAPRTooltip/index.js
+++ parts/Compound/CompoundListDetail/SwapAPRInfo/SwapAPRTooltip/index.js
@@ -19,7 +19,8 @@ const SwapAPRTooltip = () => {
 
   return (
     <div className={classes.info}>
-      <Typography variant="body1">
+      <Typography variant="h6">Swap APR</Typography>
+      <Typography variant="body2">
         Swap APR is the return you receive from the liquidity you provided being
         used in trades. Swap APR takes the form of your LP tokens being
         redeemable for more of the underlying assets.
