280cfa5a0fb786497917ba89cf22b69616ee3a4b
Jonasslv <jowsba@hotmail.com>
2022-04-04 15:34:38 -0300

    format file

--- utils/helpers/getZapperContract.js
+++ utils/helpers/getZapperContract.js
@@ -1,27 +1,27 @@
 import { CONTRACTS } from 'config';
 
 export const getZapperContract = (item) => {
-    let zapperAddress;
-    switch(item.source) {
-      case "Trader Joe":
+  let zapperAddress;
+  switch (item.source) {
+    case "Trader Joe":
+      zapperAddress = CONTRACTS.SNOWGLOBE_ZAPPER_TJ
+      break;
+    case "Pangolin":
+      zapperAddress = CONTRACTS.SNOWGLOBE_ZAPPER_PG
+      break;
+    case "Axial":
+      if (item.name === "AVAX-AXIAL") {
         zapperAddress = CONTRACTS.SNOWGLOBE_ZAPPER_TJ
         break;
-      case "Pangolin":
-        zapperAddress = CONTRACTS.SNOWGLOBE_ZAPPER_PG
+      }
+    //we want to raise if its not avax-axial
+    case "Vector":
+      if (item.name === "xPTP-PTP") {
+        zapperAddress = CONTRACTS.SNOWGLOBE_ZAPPER_TJ
         break;
-      case "Axial":
-        if (item.name === "AVAX-AXIAL") {
-          zapperAddress = CONTRACTS.SNOWGLOBE_ZAPPER_TJ
-          break;
-        }
-      //we want to raise if its not avax-axial
-      case "Vector":
-        if (item.name === "xPTP-PTP") {
-          zapperAddress = CONTRACTS.SNOWGLOBE_ZAPPER_TJ
-          break;
-        }
-        default:
-          throw new Error("Protocol is not zappable");      
-    }
-    return zapperAddress;
+      }
+    default:
+      throw new Error("Protocol is not zappable");
+  }
+  return zapperAddress;
 }
