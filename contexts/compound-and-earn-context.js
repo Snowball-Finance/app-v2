ad4df2f1ada827a0b818fd9159e27c642513d9d7
Jonas <20801365+Jonasslv@users.noreply.github.com>
2022-04-04 15:40:45 -0300

    Add vector in the routers switch (#462)
    * add vector in the routers switch
    
    * changed another declaration
    
    * added xptp to zap logic
    
    * format file

--- contexts/compound-and-earn-context.js
+++ contexts/compound-and-earn-context.js
@@ -438,7 +438,7 @@ export function CompoundAndEarnProvider({ children }) {
       } else {
         let routerAddress;
         switch(item.source) {
-          case "Trader Joe": case "Axial":
+          case "Trader Joe": case "Axial": case "Vector":
             routerAddress = CONTRACTS.ROUTER_TRADERJOE;
             break;
           case "Pangolin":
@@ -593,7 +593,7 @@ export function CompoundAndEarnProvider({ children }) {
 
     let routerAddress;
     switch(item.source) {
-      case "Trader Joe": case "Axial":
+      case "Trader Joe": case "Axial": case "Vector":
         routerAddress = CONTRACTS.ROUTER_TRADERJOE;
         break;
       case "Pangolin":
